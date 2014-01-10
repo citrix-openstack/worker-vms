5c38c5ff4c735ecc9e92b462dea0a825ba03b488
Mate Lakat <mate.lakat@citrix.com>
2014-01-10 10:37:18 +0000

    Support ssh protocol to store images

--- lib/update_worker_image.sh
+++ lib/update_worker_image.sh
@@ -35,6 +35,16 @@ function export_vm() {
         rm -f "$local_image_path" || true
         xe vm-export filename="$local_image_path" compress=True uuid=$VM
         umount $tmpdir
+    elif [ "$protocol" = "ssh" ]; then
+        local remote_host
+        local path_to_image
+
+        remote_host=$(echo "$IMAGE_LOCATION" | cut -d ":" -f 2)
+        path_to_image=$(echo "$IMAGE_LOCATION" | cut -d ":" -f 3)
+        {
+            echo "dd of=$path_to_image"
+            xe vm-export filename= compress=True uuid=$VM
+        } | remote_bash "$remote_host"
     else
         die_with << EOF
 ERROR: unrecognised protocol: $protocol specified by: $IMAGE_LOCATION
