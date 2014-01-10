d4372b9c8efffb35e84fa3b00a5145c0c79db7e1
Mate Lakat <mate.lakat@citrix.com>
2014-01-10 18:00:16 +0000

    Fix worker destruction
    Unfortunately, vm-uninstall does not destroy all of the vdis. This fix
    lists the connected vdis, and destroys them separately.

--- lib/delete_worker.sh
+++ lib/delete_worker.sh
@@ -4,4 +4,15 @@ set -eu
 
 VM_NAME="$1"
 
+VM=$(xe vm-list name-label="$VM_NAME" --minimal)
+
+VDIS="$(xe vbd-list vm-uuid=$VM params=vdi-uuid --minimal)"
+
 xe vm-uninstall vm="$VM_NAME" force=True
+
+echo "Destroying VDIS: $VDIS"
+IFS=","
+for vdi in $VDIS; do
+    echo "destroy $vdi"
+    xe vdi-destroy uuid=$vdi || true
+done
